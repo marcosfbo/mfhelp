6a26d62f61263206f619ff3218309bfbea87e2c8
Marcos Fernando <marcosfbo@gmail.com>
2022-07-20 23:24:16 -0300

    Inserindo novos chamados - bcdados

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,11 +1,47 @@
+import React from 'react';
+import { Alert } from 'react-native';
+import { useState } from 'react';
 import { VStack } from 'native-base';
+import { useNavigation } from '@react-navigation/native';
+import firestore from '@react-native-firebase/firestore';
 
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 
-
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState('');
+  const [description, setDescription] = useState('');
+
+  const navegation = useNavigation();
+
+  function handleNewOrderRegister(){
+    if(!patrimony || !description){
+      return Alert.alert('Registrar', 'Preencha todos os campos.');
+    }
+
+    setIsLoading(true);
+    firestore()
+    .collection('orders')
+    .add({
+      patrimony,
+      description,
+      status: 'open',
+      created_at: firestore.FieldValue.serverTimestamp()
+    })
+    .then(() => {
+      Alert.alert("Solicitação", "Chamado registrado com sucesso.");
+      navegation.goBack();
+    })
+    .catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+      return Alert.alert('Solicitação', 'Não foi possível registrar o chamado.');
+    });
+    
+  }
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
 
@@ -14,6 +50,7 @@ export function Register() {
         <Input
             placeholder="Número do patrimônio"
             mt={4}
+            onChangeText={setPatrimony}
         />
 
         <Input
@@ -22,6 +59,7 @@ export function Register() {
             mt={5}
             multiline
             textAlignVertical="top"
+            onChangeText={setDescription}
         />
 
         <Button
