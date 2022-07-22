b15f48e34d841a11441981133e8cae1d28579d9e
Marcos Fernando <marcosfbo@gmail.com>
2022-07-21 23:05:14 -0300

    Detalhes do Chamado

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from 'react';
+import { VStack, Text, HStack, useTheme, ScrollView } from 'native-base';
 import { useRoute } from '@react-navigation/native';
-import { VStack, Text } from 'native-base';
+import firestore from '@react-native-firebase/firestore';
+import { OrderFirestoreDTO } from '../DTOs/OrderFirestoreDTO';
+import { dateFormat } from '../utils/firestoreDataFormat';
+import { CircleWavyCheck, Hourglass, DesktopTower, Clipboard } from 'phosphor-react-native';
+
+import { Button} from '../components/Button';
+import { Input } from '../components/Input';
 import { Header } from '../components/Header';
 import { OrderProps } from '../components/Order';
+
+import { Loading } from '../components/Loading';
+import { CardDetails } from '../components/CardDetails';
+import React from 'react';
 
 type RouteParams = {
   orderId: string;
@@ -15,18 +26,98 @@ type OrderDetails = OrderProps & {
 }
 
 export function Details() {
+  const [solution, setSolution] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [order, setOrder] = useState<OrderDetails>({} as OrderDetails);
 
+  const { colors } = useTheme();
   const route = useRoute();
   const {orderId} = route.params as RouteParams;
+
+  useEffect(() => {
+    firestore()
+    .collection<OrderFirestoreDTO>('orders')
+    .doc(orderId)
+    .get()
+    .then((doc) => {
+      const { patrimony, description, status, created_at, closed_at, solution} = doc.data();
+
+      const closed = closed_at ? dateFormat(closed_at) : null;
+
+      setOrder({
+        id: doc.id,
+        patrimony,
+        description,
+        status,
+        solution,
+        when: dateFormat(created_at),
+        closed
+      });
+
+      setIsLoading(false);
+    });
+  }, [])
+
+  if(isLoading) {
+    return(<Loading />);
+  }
 
   return (
     <VStack flex={1} bg="gray.700">
         <Header title="solicitação"/>
-        <Text color="white">
-          {orderId}
-        </Text>
+
+        <HStack bg="gray.500" justifyContent="center" p={4}>
+          {
+            order.status === 'closed'
+              ? <CircleWavyCheck size={22} color={colors.green[300]} />
+            : <Hourglass size={22} color={colors.secondary[700]} />
+          }
+
+          <Text 
+          fontSize="sm"
+          color={order.status === 'closed' ? colors.green[300] : colors.secondary[700]}
+          ml={2}
+          textTransform="uppercase"
+          >
+            {order.status === 'closed' ? 'finalizado' : 'em andamento'}
+          
+          </Text>
+        </HStack>
+
+        <ScrollView mx={5} showsVerticalScrollIndicator={false}>
+          <CardDetails
+            title="equipamento"
+            description={`Patrimônio ${order.patrimony}`}
+            icon={DesktopTower}
+            footer={order.when}
+          />
+          <CardDetails
+            title="descrição do problema"
+            description={order.description}
+            icon={Clipboard}
+          />
+          <CardDetails
+            title="solução"
+            icon={CircleWavyCheck}
+            footer={order.closed && `Encerrado em ${order.closed}`}
+          >
+            <Input
+              placeholder="Descrição da solução"
+              onChangeText={setSolution}
+              h={24}
+              textAlignVertical="top"
+              multiline
+            />
+          </CardDetails>
+        </ScrollView>
+
+        {
+          order.status === 'open' &&
+            <Button 
+              title="Encerrar chamado"
+              margin={5}
+            />
+        }
     </VStack>
   );
 }
